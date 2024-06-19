baa5e9b05fd63f02efed8297341c9354100a92d5
Ben Johnke <ben.a.johnke@gmail.com>
2024-06-19 10:51:02 -0600

    Rename main controller and associated GET URL

--- RedditTopPostsAndUsers/Controllers/StatisticsController.cs
+++ RedditTopPostsAndUsers/Controllers/StatisticsController.cs
@@ -4,21 +4,21 @@ namespace RedditTopPostsAndUsers.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class StatisticsController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<StatisticsController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public StatisticsController(ILogger<StatisticsController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetStatistics")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
